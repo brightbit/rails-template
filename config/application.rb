5796e7925d672c324b65876c90ae15a602feb8bb
Eric <ericboehs@gmail.com>
2013-11-20 18:07:15 -0600

    Add logging to STDOUT for heroku/unicorn

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module MyApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Tell Unicorn to log to STDOUT on Heroku
+    config.logger = Logger.new(STDOUT) unless Rails.env.development?
   end
 end
