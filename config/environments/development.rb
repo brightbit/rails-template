c2257d58d7d90720890de0b89bb4b8d2b45ef7d0
Eric Boehs <ericboehs@gmail.com>
2013-11-22 13:52:05 -0600

    Use memcache in development and production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,10 @@ MyApp::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+
+  # Cache stuff in memcache
+  config.cache_store = :mem_cache_store, { expires_in: 10.minutes }
 
   # Open email in browser or send via smtp
   if ENV['EMAIL_RECIPIENTS']
